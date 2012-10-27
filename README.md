307e90baeac974c3571212687955313be85c89a8
Lee Goddard <leegee@gmail.com>
2012-10-27 16:13:48 +0200

    Docs

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Mootools-Knob
 
 Provides a rotary knob control for Mootools
 
-![Screenshot](https://raw.github.com/leegee/Mootools-Knob/master/Docs/screenshot.png)
+![Screenshot](https://raw.github.com/leegee/Mootools-Knob/master/Demo/screenshot.png)
 
 How to use
 ----------
@@ -106,51 +106,57 @@ Options
 The following options are available. Some may be supplied as 
 *dataset-* attributes when their values are literal.
 
-* **element**: the DOM element to replace with this control
-* **value**: (*0*) The iInitial value of the control. If not supplied, taken from attributes *value* or *data-value*
-* **range**: (*[-100, 100]*) The minimum and maximum values. May be supplied in HTML as *aria-valuemin* and *aria-valuemax*.
-* **scale**: (*1*) Multiplier applied to the number of pixels the mosue may be moved, to acheive change in the **value** field (see Events, below)
-* **keychangeby**: (*1*) When arrow keys control the knob, the **value** field is increased by this factor
-* **keychangebywithshift**: (*10*) As **keychangeby**, above, but for when shift is held.
-* **monitor**: A name for, or instance of, a DOM element that has a **value** field that shoudl be monitored for changes, to be reflected by this control. Intended for text input elements.
-* **monitorMs**: (*250*) The interval, in milliseconds, at which to check the **monitor** element, if supplied. The monitor element's *value* (an aria-related) attribute will also be updated with changes to the control.
-* **addpointer**: (*↑*) By default the module (since 0.3) replaces **element**'s content with  an up-arrow to the element. Disable this by setting this option to null or false.
-* **forceint**:	(*false*) Force all values to be integers
+* `element`: the DOM element to replace with this control
+* `value`: (*0*) The iInitial value of the control. If not supplied, taken from attributes *value* or *data-value*
+* `range`: (*[-100, 100]*) The minimum and maximum values. May be supplied in HTML as *aria-valuemin* and *aria-valuemax*.
+* `scale`: (*1*) Multiplier applied to the number of pixels the mosue may be moved, to acheive change in the `value` field (see Events, below)
+* `keychangeby`: (*1*) When arrow keys control the knob, the `value` field is increased by this factor
+* `keychangebywithshift`: (*10*) As `keychangeby`, above, but for when shift is held.
+* `monitor`: A name for, or instance of, a DOM element that has a `value` field that shoudl be monitored for changes, to be reflected by this control. Intended for text input elements.
+* `monitorMs`: (*250*) The interval, in milliseconds, at which to check the `monitor` element, if supplied. The monitor element's *value* (an aria-related) attribute will also be updated with changes to the control.
+* `addpointer`: (*↑*) By default the module (since 0.3) replaces `element`'s content with  an up-arrow to the element. Disable this by setting this option to null or false.
+* `forceint`:	(*false*) Force all values to be integers
+* `degreesoffset`: (*0*) Offset, in degrees, to apply to the rotation of the knob. This does not effect the value of the control, only its appearance, and can change the style of the control form a pan control to a volume control, for example. The default is *0* but this may change.
 
 Events
 ------
 
 In addition to the above options, the following events are supplied:
 
-* **onMousedown**: fired when the knob is clicked
-* **onMouseup**: fired when the knob is released but before it is rendered
-* **onTick**: fired as the knob is turned
+* `onMousedown`: fired when the knob is clicked
+* `onMouseup`: fired when the knob is released but before it is rendered
+* `onTick`: fired as the knob is turned
 
-The **onTick** event is intended to allow the user to adjust the behaviour 
+The `onTick` event is intended to allow the user to adjust the behaviour 
 of the widget, using the following object fields:
 
-* **x** and **y** represent the position of the mouse curosr relative to the knob
-* **movement**  contains the greater of these two, 
-* **value** contains the previous value incremented by **movement** multiplied by the value of the **scale** option (*this.options.scale*).
-* **degrees** contains the amount by which the knob will be rotated, and can be set in accordance with the values accepted by the CSS3 Transform/rotate property (0-360, afik)
+* `x` and `y` represent the position of the mouse curosr relative to the knob
+* `movement`  contains the greater of these two, 
+* `value` contains the previous value incremented by `movement` multiplied by the value of the `scale` option (*this.options.scale*).
+* `degrees` contains the amount by which the knob will be rotated, and can be set in accordance with the values accepted by the CSS3 Transform/rotate property (0-360, afik)
 	
-The **onTick** event could just be used to update a text display field.
+The `onTick` event could just be used to update a text display field.
 
 Public Methods
 --------------
 
-* **render**: Update the control to reflect the current state of the **value** field, which may be set by supply a single, numeric argument
-* **attach**: Called at instantiation to attach eveents to allow the control to operate
-* **detach**: Removes events
+* `render`: Update the control to reflect the current state of the `value` field, which may be set by supply a single, numeric argument
+* `attach`: Called at instantiation to attach eveents to allow the control to operate
+* `detach`: Removes events
+
+TO DO
+-----
+
+* Double click support is nearly available, but how to convert an angle to a value?
 
 FAQ
 ---
 
-**When using HTML mark-up to create a knob, why do *valuemin* and *valuemax* not work?**
+`When using HTML mark-up to create a knob, why do *valuemin* and *valuemax* not work?`
 
-The acceptable range of inputs should be specified through **aria-valuemin** and **aria-valuemax*, not **data-valuemin** and **data-valuemax**.
+The acceptable range of inputs should be specified through `aria-valuemin` and `aria-valuemax*, not `data-valuemin` and `data-valuemax`.
 
-**Why are arguments all lower case, rather than camel case?**
+`Why are arguments all lower case, rather than camel case?`
 
-To make parsing of **dataset** attributes easier: these are all forced to lower case by the browser.
+To make parsing of `dataset` attributes easier: these are all forced to lower case by the browser.
 
--- Source/Knob.js
+++ Source/Knob.js
@@ -1,6 +1,6 @@
 /*
 ---
-description: A rotary knob input control
+description: Rotary-knob input control with WIA-ARIA and keyboard support
 
 license: MIT-style
 
@@ -16,56 +16,96 @@ provides: [Knob]
 ...
 */
 
-/*
-	Version 0.2 - WIA-ARIA and keyboard control Support
+// # MooKnob
+//	Version 0.5 - degreesoffset, nearly got double-click support, 
+//	Version 0.4 - Default pointer
+//	Version 0.2 - WIA-ARIA and keyboard control Support
+//
+//	This code is copyright (C) 2012 Lee Goddard, Server-side Systems Ltd.
+//	All Rights Reserved.
+//	
+//	Available under the same terms as Perl5.
+//
+//	Provides events:
+//	
+//		onMousedown - when the knob is clicked
+//		onTick - every time the knob turns
+//		onMouseup - when the knob is released
 
-	This code is copyright (C) 2012 Lee Goddard, Server-side Systems Ltd.
-	All Rights Reserved.
-	
-	Available under the same terms as Perl5.
 
-	Provides events:
-	
-		onMousedown - when the knob is clicked
-		onTick - every time the knob turns
-		onMouseup - when the knob is released
-*/
-
+// ## Disgusting global variable
+// Thhis horrible global variable seems to be necessary 
 var __ActiveMooToolsKnobCtrl__ = null;
 
+// ## Class definition
 var Knob = new Class({
 	Implements: [Options, Events],
-	
-	options: {					// Keep options lower-case for dataset compatability
-		element: 		null,	/* DOM element to replace with this control */
-		value:			0,		/* Initial value of control: if not supplied, taken from attributes 'value' or 'data-value' */
-		addpointer:		'↑',		/* Default is an up-arrow (↑). If you set this to null, the up-arrow is not added to the knob element */
-		range:			[-100, 100], 	/* Minimum and maximum values */
-		scale:			1,		/* Multiplier applied to number of px moved, to acheive change in .value */
-		keychangeby:				1, 	/* When arrow keys control knob, incrase knob value by this */
-		keychangebywithshift: 10, /* As keyUnit but for when shift key is also pressed */
-		forceint:	false,	/* Force all values to be integers */
-		monitor:		null, /* May be a string or DOM element to monitor: changes in this elements *value* attribute will change the control's *value* attribute, and cause the control to be re-rendered. */
-		monitorMs:	1000/4, /* Frequency of checking for monitor.value changes */
+
+// ### Options
+
+	// Keep options lower-case for dataset compatability
+	options: {						
+		// DOM element or element ID, to replace with this control 
+		element: 		null,		
+		// Initial value of control: if not supplied, taken from attributes 'value' or 'data-value' 
+		value:			0,			
+		// Default is an up-arrow (↑). Text to add within knob element. If you set this to null, the up-arrow is not added to the knob element 
+		addpointer:		'↑',	
+		// Minimum and maximum values 		
+		range:			[-100, 100], 
+		// Multiplier applied to number of px moved, to acheive change in .value 
+		scale:			1,			
+		// When arrow keys control knob, incrase knob value by this 
+		keychangeby:				1, 	
+		// As keyUnit but for when shift key is also pressed 
+		keychangebywithshift: 10, 	
+		// Force all values to be integers 
+		forceint:	false,			
+		// May be a string or DOM element to monitor: changes in this elements *value* attribute will change the control's *value* attribute, and cause the control to be re-rendered. 
+		monitor:		null, 			
+		// Frequency of checking for monitor.value changes 
+		monitorMs:	1000/4, 		
+		// Adjusts rotation by degrees: changes the knob type from a pan control to a 0-10 control, for eample.
+		degreesoffset:	0,
 		
+		// Fired when all processing is done, but for rotation the control by the value in `this.degrees`
 		onTick:			function(){},
+		// Fired when the main mouse button is depressed, but after processing
 		onMousedown: 	function(){},
+		// Fired when the main mouse button is released, but after processing
 		onMouseUp:		function(){}
 	},
 
-	element:			null,
-	monitor:			null,	/* See options.monitor */
-	monitorOldValue: null,
-	monitorTimer:	null,	/* setInterval timer for checking monitor.value */
-	movement:		null,	/* The Euclidean distance of dragged cursor from origin in element  */
-	movementAnchor:	null,	/* Position of element at knob mouse down */
-	value:			null,	/* Actual value of control */
-	initialValue:	null,	/* Cache of 'value', prior to drag starts */
-	finalValue:		null,	/* When drag ends and is not canceled */
-	dragging:		false,	/* Flag */
-	renderRange:		null,	/* For rendering */
-	// dblClickAnchor:	null,	/* Position for double-click-to-value */
+	// ### Other fields
 	
+	// DOM element: ontains the control: see `options.element`
+	element:			null,
+	// DOM element: See `options.monitor`
+	monitor:			null,	
+	// Previous value of the monitor element 
+	monitorOldValue: null,
+	// setInterval timer for checking monitor.value 
+	monitorTimer:	null,	
+	// The Euclidean distance of dragged cursor from origin in element  
+	movement:		null,	
+	// Position of element at knob mouse down 
+	movementAnchor:	null,	
+	// Actual value of control 
+	value:			null,	
+	// Cache of 'value', prior to drag starts 
+	initialValue:	null,	
+	// When drag ends and is not canceled 
+	finalValue:		null,	
+	// Flag 
+	dragging:		false,	
+	// For rendering 
+	renderRange:		null,	
+	/* // Positioning info for double-click-to-value 
+	 dblClickAnchor:	null,	
+	*/
+
+// ### Methods
+
 	initialize: function( options, actx ){
 		var self = this;
 		this.setOptions(options);
@@ -74,13 +114,13 @@ var Knob = new Class({
 			document.id(this.options.element) 
 			: this.element = this.options.element;
 		
-		// console.debug('Initialising knob '+this.element.id);
-			
+		// Required for keyboard focus
 		this.element.setAttribute('tabIndex', this.element.getAttribute('tabIdex') || 0);
 
-		if (this.options.addpointer){
+		// I was adding this manually a lot, and it was tedious
+		// to keep looking-up UTF-8 arrow characters
+		if (this.options.addpointer)
 			this.element.set('text', this.options.addpointer );
-		}
 		
 		if (this.options.monitor){
 			this.monitor = (typeof this.options.monitor == 'string')?
@@ -88,11 +128,6 @@ var Knob = new Class({
 				: this.monitor = this.options.monitor;
 		}
 	
-		this.element.addEvents({
-			focus: this.focus,
-			blur: this.blur
-		});
-
 		var block = this.element.getStyle('display');
 		if (block=='inline' || block=='')
 			this.element.setStyle('display', 'inline-block'); 
@@ -118,36 +153,83 @@ var Knob = new Class({
 		this.render(); // display initial value1
 	},
 
+	// Initiate listeners
 	attach: function(){
-	//	this.element.addEvent('dblclick', this.dblclick);
-		this.element.addEvent('mousedown', this.mousedown);
+		this.element.addEvents({
+			focus:		this.focus,
+			blur:		this.blur,
+		/*	dblclick:	this.dblclick,
+		*/
+			mousedown:	this.mousedown
+		});
 		if (this.monitor) this.monitor.addEvent('change', this.monitorValueChange);
 		this.monitorTimer = this.monitorValueChange.periodical(
 			this.options.monitorMs, this
 		);
 	},
 
-	/*	
+	// Remove listeners
+	detach: function(){
+		if (__ActiveMooToolsKnobCtrl__){
+			if (__ActiveMooToolsKnobCtrl__.monitorTimer)
+				clearInterval( __ActiveMooToolsKnobCtrl__.monitorTimer );
+			
+			if (__ActiveMooToolsKnobCtrl__.monitor) 
+				__ActiveMooToolsKnobCtrl__.monitor.removeEvent(
+					'change', __ActiveMooToolsKnobCtrl__.monitorValueChange
+				);
+
+			
+			__ActiveMooToolsKnobCtrl__.element.removeEvents({
+				focus:		__ActiveMooToolsKnobCtrl__.focus,
+				blur: 	 	__ActiveMooToolsKnobCtrl__.blur,
+			/*	dblclick: 	__ActiveMooToolsKnobCtrl__.dblclick,
+			*/
+				mousedown: 	__ActiveMooToolsKnobCtrl__.mousedown
+			});
+			
+			if (__ActiveMooToolsKnobCtrl__.dragging) 
+				window.removeEvent('mouseup', __ActiveMooToolsKnobCtrl__.mouseup);
+		 	
+			__ActiveMooToolsKnobCtrl__.element.removeEvent('focus',__ActiveMooToolsKnobCtrl__.focus);
+		 	__ActiveMooToolsKnobCtrl__.element.removeEvent('blur', __ActiveMooToolsKnobCtrl__.blur);
+			
+		 	window.removeEvent('mousemove', __ActiveMooToolsKnobCtrl__.mousemove);
+		}
+	},
+	
+	destroy: function(){ this.detach() },
+
+// Double-click the control to set it's value:
+// this would make my GUI a lot easier, if only 
+// I could convert the angle to a value.
+
+/*
 	dblclick: function(e){
 		var self = this.retrieve('self');
 		if (!self.dblClickAnchor) 
 			self.dblClickAnchor = self.element.getCoordinates();
-		// Co-ords of click within element:
-		var x = e.page.x - self.dblClickAnchor.left;
-		var y = e.page.y - self.dblClickAnchor.top;
-		// Centre of element is width/2, height/2
-		// Angle between centre and click:
-		var theta = self.angleBetween2Points(
-			[x,y],
-			[self.dblClickAnchor.width/2, self.dblClickAnchor.height/2]
+		var theta = self.angleBetween2Points( 		// Find the angle between the element centre and the click:
+			[e.page.x - self.dblClickAnchor.left, e.page.y - self.dblClickAnchor.top ], // Co-ords of click within element:
+			[self.dblClickAnchor.width/2, self.dblClickAnchor.height/2] // Centre of element is width/2, height/2
 		);
-		if (theta < 0) theta += 2 * Math.PI;
-		var degrees = theta * (180 / Math.PI);
-		degrees += 90;
-		if (degrees > 360) degrees -= 360;
-		console.log( degrees );
+		if (theta < 0) theta += 2 * Math.PI;		// Keep radians positive
+		var degrees = theta * (180 / Math.PI);	// Convert to degrees
+		degrees *= -1;							// Invert to display
+
+		console.log( degrees +' '+(degrees*-1) );		// degrees += self.options.degreesoffset;
+		
+		? this.value = (degrees+this.options.degreesoffset) / this.renderRange;
+
+		self.element.setStyles({ 
+			'transform': 'rotate('+degrees+'deg)',
+			'-ms-transform': 'rotate('+degrees+'deg)',
+			'-webkit-transform': 'rotate('+degrees+'deg)',
+			'-o-transform': 'rotate('+degrees+'deg)',
+			'-moz-transform': 'rotate('+degrees+'deg)',
+		});
 	},
-	*/
+*/
 	
     angleBetween2Points: function ( point1, point2 ) {
         var dx = point2[0] - point1[0];
@@ -155,7 +237,7 @@ var Knob = new Class({
         return Math.atan2( dx, dy );
     },
 	
-	/* Monitor changes in the .monitor field's value, and update control */
+// Monitor changes in the .monitor field's value, and update control
 	monitorValueChange: function(e){
 		if ( this.monitor ){
 			var v = parseFloat( this.monitor.get('value') );
@@ -167,20 +249,6 @@ var Knob = new Class({
 		}
 	},
 
-	destroy: function(){ this.detach() },
-	
-	detach: function(){
-		if (this.monitorTimer) clearInterval( this.monitorTimer );
-		this.element.removeEvent('mousedown', this.mousedown);
-		if (this.dragging) 
-			window.removeEvent('mouseup', __ActiveMooToolsKnobCtrl__.mouseup);
-		if (__ActiveMooToolsKnobCtrl__){
-		 	__ActiveMooToolsKnobCtrl__.element.removeEvent('focus',__ActiveMooToolsKnobCtrl__.focus);
-		 	__ActiveMooToolsKnobCtrl__.element.removeEvent('blur', __ActiveMooToolsKnobCtrl__.blur);
-			window.removeEvent('mousemove', __ActiveMooToolsKnobCtrl__.mousemove);
-		}
-	},
-	
 	focus: function(e){
 		var self;
 		if (e) e.stop();
@@ -189,7 +257,6 @@ var Knob = new Class({
 		else self = this;	
 		__ActiveMooToolsKnobCtrl__ = self;
 		window.addEvent('keydown', self.keydown);
-		// console.debug('Focused '+self.element.id);
 	},
 
 	blur: function(e){
@@ -199,11 +266,11 @@ var Knob = new Class({
 		__ActiveMooToolsKnobCtrl__ = null;
 	},
 
-	/* When the control has focus, the arrow keys change the value
-		by the amount specified in the options. Use of the shift key
-		can optionally change increase the change, by default by a 
-		factor of ten. Use of meta/ctrl/alt maimises or minimises
-		the value within the specified range. */
+// When the control has focus, the arrow keys change the value
+// by the amount specified in the options. Use of the shift key
+// can optionally change increase the change, by default by a 
+// factor of ten. Use of meta/ctrl/alt maimises or minimises
+// the value within the specified range.
 	keydown: function(e){
 		var self = __ActiveMooToolsKnobCtrl__;
 		var change;
@@ -236,21 +303,22 @@ var Knob = new Class({
 	},
 
 	mousedown: function(e){
-		// Get element position here, not earlier, to allow for resizing:
 		e.stop();
 		var self = this.retrieve('self');
 		__ActiveMooToolsKnobCtrl__ = self;
 		self.element.focus();
 		self.focus();		
+		// Get element position here, not earlier, to allow for client resizing:
 		self.movementAnchor = this.getPosition();
 		self.initialValue = self.value;
 		window.addEvent('mousemove', self.mousemove );
-		// How to maintain lexical context?
 		window.addEvent('mouseup', self.mouseup );
 		self.dragging = true;
 		self.fireEvent('mousedown');
 	},
-	
+
+// Unsure how to bind the mouseup event to an object,
+// hence the ugly global
 	mouseup: function(e){
 		var self = __ActiveMooToolsKnobCtrl__;
 		e.stop();
@@ -260,17 +328,16 @@ var Knob = new Class({
 		self.fireEvent('mouseup');
 	},
 	
-	/* Sets the x, y field as the position of the mouse curosr relative to the knob,
-	   sets the movement field as the greater of these two, 
-	   and increments the value field by 'movement' multiplied by the value of
-	   the scale field. 
-	   If there is a range field, and the value falls outside of it,
-	   the value is constrained.
-	   The value field is passed to the degree field,
-	   and then the onTick event is fired, to allow something interesting
-	   to be done with these fields, prior to the CSS 'rotate' transformation,
-	   which uses the degree field.
-	*/
+// Sets the x, y field as the position of the mouse curosr relative to the knob,
+// sets the movement field as the greater of these two, 
+// and increments the value field by 'movement' multiplied by the value of
+// the scale field. 
+// If there is a range field, and the value falls outside of it,
+// the value is constrained.
+// The value field is passed to the degree field,
+// and then the onTick event is fired, to allow something interesting
+// to be done with these fields, prior to the CSS 'rotate' transformation,
+// which uses the degree field.
 	mousemove: function(e){
 		var self = __ActiveMooToolsKnobCtrl__;
 		if (!self.dragging) return;
@@ -284,18 +351,17 @@ var Knob = new Class({
 		self.x = e.page.x - self.movementAnchor.x;
 		self.y = e.page.y - self.movementAnchor.y;
         
-		// var d = Math.sqrt(  Math.pow(self.movementAnchor.x + self.x, 2)  + Math.pow(self.movementAnchor.y + self.y, 2)  );
+		/* var d = Math.sqrt(  Math.pow(self.movementAnchor.x + self.x, 2)  + Math.pow(self.movementAnchor.y + self.y, 2)  ); */
 		
 		self.movement = (Math.abs(self.x) > Math.abs(self.y)? self.x : self.y);
 		self.value    = self.initialValue + ( self.movement * self.options.scale);
 		self.render();
 	},
 	
-	/* Rotates the control knob.
-		Requires this.value to be set.
-		Sets this.degrees, and element's aria-valuenow/-valuetext.
-		If a parameter is supplied, it sets this.value
-	*/
+// Rotates the control knob.
+// Requires this.value to be set.
+// Sets this.degrees, and element's aria-valuenow/-valuetext.
+// If a parameter is supplied, it sets this.value
 	render: function(v){
 		if (typeof v != 'undefined') this.value = parseFloat( v );
 
@@ -319,29 +385,34 @@ var Knob = new Class({
 			this.monitor.set('aria-valuetext', this.value);
 		}
 
-		this.degrees = this.value * (360 / this.renderRange);
 		this.element.set('aria-valuenow', this.value);
 		this.element.set('aria-valuetext', this.value);
+
+		this.degrees = (this.value * (360 / this.renderRange))
+			+ this.options.degreesoffset;
+		
 		this.fireEvent('tick');
 
 		this.element.setStyles({ 
-			'transform': 'rotate('+this.degrees+'deg)',
-			'-ms-transform': 'rotate('+this.degrees+'deg)',
-			'-webkit-transform': 'rotate('+this.degrees+'deg)',
-			'-o-transform': 'rotate('+this.degrees+'deg)',
-			'-moz-transform': 'rotate('+this.degrees+'deg)',
+			'transform': 		 'rotate(' + this.degrees + 'deg)',
+			'-ms-transform': 	 'rotate(' + this.degrees + 'deg)',
+			'-webkit-transform':	 'rotate(' + this.degrees + 'deg)',
+			'-o-transform': 		 'rotate(' + this.degrees + 'deg)',
+			'-moz-transform': 	 'rotate(' + this.degrees + 'deg)'
 		});
 	}
 });
 
-/*
-	The range of a widget defaults to +/-100,
-	but can be set with the aria-valuemin and aria-valuemax attributes 
-	of the element. The initial value can be set via the value attribute
-	or data-value attribute. The chosen value of the control will be placed
-	in the aria-valuenow and aria-valuetext attributes, and in the value
-	attribute, if present, otherwise in the data-value attribute.
-*/
+// ## Knob.parseDOM(*selector*)
+// Converts to a Knob any element matching *selector*, which
+// defaults to `.mooknob`.
+//	The range of a widget defaults to +/-100,
+//	but can be set with the `aria-valuemin` and `aria-valuemax` attributes 
+//	of the element. The initial value can be set via the `value` attribute
+//	or `data-value` attribute. The chosen value of the control will be placed
+//	in the `aria-valuenow` and `aria-valuetext` attributes, and in the `value`
+//	attribute, if present, otherwise in the `data-value` attribute.
+// *Throws* a `OperatorError` if only one of aria-valuemin and aria-valuemax are set.
 Knob.parseDOM = function( selector ){
 	selector = selector || '.mooknob';
 	$$(selector).each( function(el){
@@ -349,7 +420,8 @@ Knob.parseDOM = function( selector ){
 			element:	 el,
 		};
 		Object.keys(el.dataset).each( function(i){
-			try { // rough casting
+			// Rough casting 
+			try { 
 				opts[i] = eval( el.dataset[i] )
 			}
 			catch(e) {
@@ -357,6 +429,7 @@ Knob.parseDOM = function( selector ){
 				if (e.toString().match(/ReferenceError/)){
 					opts[i] = el.dataset[i];
 				} 
+				// Real issues
 				else {
 					console.log('Error setting '+i+' from dataset');
 					console.info(e);
@@ -366,21 +439,31 @@ Knob.parseDOM = function( selector ){
 		
 		var min = el.get('aria-valuemin') || null;
 		var max = el.get('aria-valuemax') || null;
+		
 		if (min || max){
 			if (min && !max)
-				throw('aria-valuemin without aria-valuemax: default range will be used');
+				throw new OperatorError('aria-valuemin without aria-valuemax: default range will be used');
 			else if (max && !min) 
-				throw('aria-valuemax without aria-valuemin: default range will be used');
+				throw new OperatorError('aria-valuemax without aria-valuemin: default range will be used');
 			else 
 				opts.range = [min,max];
 		}
 		opts.value = el.get('value') || el.dataset.value || 0;
 		
-		// console.debug('Create knob '+el.id);
 		new Knob(opts);
-	});
-}	
+	}); // Next element to convert
+}
 
+
+// ## OperatorError
+// Exception object for your pleasure.
+function OperatorError(message) {
+   this.message = message;
+   this.name = "OperatorError";
+}
+
+// ## Finally
+// Que our parse of the DOM
 document.addEvent('domready', function(){
 	Knob.parseDOM();	
 });
